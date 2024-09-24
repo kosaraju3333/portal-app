14a5c827ae62e34eec5b6aed0756d28e7daae8d4
ramakrishna <kosaraju3333@gmail.com>
2024-09-24 12:15:40 +0530

    added aws codedeploy config

---
+++ scripts/start-portal-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo service portal-app restart
+
+
+#!/bin/bash
+set -e 
+
+sleep 5
+
+is_service_up=`pm2 list | sed -n "4p" | awk -F " " '{print$4}'`
+
+if [ "$is_service_up" = "portal-app" ]
+then
+   echo "$is_service_up is already exists so now restaring the $is_service_up"
+   sudo service portal-app restart
+   break
+else
+   echo "portal-app is not exists so now staring the portal-app"
+   sudo service portal-app start
+fi
---
+++ scripts/stop-portal-app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Stopping portal-app"
+
+sudo service portal-app stop
